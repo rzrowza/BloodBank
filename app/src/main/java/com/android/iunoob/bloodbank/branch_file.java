ee6c988fbca252055230e98c43ce53db64643377
Rowzatul Zannat <w.rzrowza@gmail.com>
2022-05-15 19:42:36 +0600

    branch file added

---
+++ app/src/main/java/com/android/iunoob/bloodbank/branch_file.java
@@ -0,0 +1,17 @@
+package com.android.iunoob.bloodbank;
+
+public class branch_file {
+    public int x;
+
+    public branch_file(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
