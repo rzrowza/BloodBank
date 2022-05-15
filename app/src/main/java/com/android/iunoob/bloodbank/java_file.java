ccc91e26ab30d879c1b193489b1231cefdb10507
Rowzatul Zannat <w.rzrowza@gmail.com>
2022-05-15 19:38:13 +0600

    java file

---
+++ app/src/main/java/com/android/iunoob/bloodbank/java_file.java
@@ -0,0 +1,17 @@
+package com.android.iunoob.bloodbank;
+
+public class java_file {
+    public int x;
+
+    public java_file(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
